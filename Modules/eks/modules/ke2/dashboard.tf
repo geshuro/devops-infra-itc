d26e9da616ba14e5af29f2b73be790fce57a8d45
Isaac Mendoza <imendozak24@gmail.com>
2022-09-03 10:55:26 -0500

    actualizar helmchart dashboard

--- Modules/eks/modules/ke2/dashboard.tf
+++ Modules/eks/modules/ke2/dashboard.tf
@@ -1,16 +1,43 @@
+/*deprecado
 data "helm_repository" "stable" {
   name = "stable"
   url  = "https://kubernetes-charts.storage.googleapis.com"
-}
+}*/
 
 resource "helm_release" "dashboard" {
   count = var.install_dashboard ? 1 : 0
 
   keyring   = ""
   name      = "dashboard"
-  chart     = "stable/kubernetes-dashboard"
+  chart     = "kubernetes-dashboard/kubernetes-dashboard"
   namespace = "kube-system"
-
+  
   # depends_on = [kubernetes_deployment.tiller_deploy]
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "protocolHttp"
+    value = "true"
+  }
+
+  set {
+    name  = "service.externalPort"
+    value = 80
+  }
+
+  set {
+    name  = "replicaCount"
+    value = 2
+  }
+
+  set {
+    name  = "rbac.clusterReadOnlyRole"
+    value = "true"
+  }
+
 }
 
