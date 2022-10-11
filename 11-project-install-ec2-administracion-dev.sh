56bca68d03b469ef013ef3a219aea7ddea5a4643
Isaac Mendoza <imendozak24@gmail.com>
2022-10-11 18:11:02 -0500

    Update 11-project-install-ec2-administracion-dev.sh

--- 11-project-install-ec2-administracion-dev.sh
+++ 11-project-install-ec2-administracion-dev.sh
@@ -2,8 +2,8 @@
 cd Shared/ec2-administracion
 terraform init
 terraform workspace list
-terraform workspace new ec2-administracion-dev
-terraform workspace select ec2-administracion-dev
+terraform workspace new ec2-administracion
+terraform workspace select ec2-administracion
 terraform init
 terraform plan
 terraform apply -auto-approve
