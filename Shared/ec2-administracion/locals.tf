e779a5639000b356c93431b2515ceb00b251474d
Isaac Mendoza <imendozak24@gmail.com>
2022-10-11 18:11:53 -0500

    ireland

--- Shared/ec2-administracion/locals.tf
+++ Shared/ec2-administracion/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  region  = "us-east-1" //region donde se desplegaran la infraestructura
-  profile = "atos-integracam-tf-desarrollo" // Este valor se debe de establecer entre el equipo, porque debe configurarse el mismo para poder trabajar de forma colaborativa. Tiene el perfil con los permisos para desplegar la infraestructura
+  region  = "eu-west-1" //Region donde se desplegaran la infraestructura
+  profile = "atos-integracam-tf-desarrollo-ireland" //El perfil cuenta con los permisos para desplegar la infraestructura
 }
