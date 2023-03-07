b00a75e2adc70825ececd8e89a7327ac64c394da
Isaac <imendozak24@gmail.com>
2023-03-07 00:25:07 -0500

    delete fhir dev

--- Dev/FHIR-windowserver2019/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  region  = "us-east-1" //region donde se desplegaran la infraestructura
-  profile = "atos-integracam-tf-desarrollo" // Este valor se debe de establecer entre el equipo, porque debe configurarse el mismo para poder trabajar de forma colaborativa. Tiene el perfil con los permisos para desplegar la infraestructura
-  protocol_tcp = "tcp"
-  protocol_udp = "udp"
-}
